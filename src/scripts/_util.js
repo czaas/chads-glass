3ebe8009dd3308941339dc11ef270251b6ad2cd2
Cameron Zaas <cameronzaas@gmail.com>
2017-07-04 13:19:55 -0700

    Added infinite load to both scroll to bottom of page and when toggling through views in the modal

--- src/scripts/_util.js
+++ src/scripts/_util.js
@@ -13,3 +13,50 @@ export function getUrlParameter(sParam) {
         }
     }
 };
+
+// slightly modified so half of the element must be visible
+// https://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling#answer-22480938
+export function isScrolledIntoView(el) {
+    var boundingClientRect = el.getBoundingClientRect();
+    var elemTop = boundingClientRect.top;
+    var elemBottom = boundingClientRect.bottom;
+    var elemHeight = boundingClientRect.height;
+
+    var isVisible = (elemTop >= 0) && (elemBottom <= (window.innerHeight + (elemHeight / 2)));
+    return isVisible;
+}
+
+
+// https://davidwalsh.name/javascript-debounce-function
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+export function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+// http://sampsonblog.com/749/simple-throttle-function
+export function throttle (callback, limit) {
+    var wait = false;                 // Initially, we're not waiting
+    return function () {              // We return a throttled function
+        if (!wait) {                  // If we're not waiting
+            callback.call();          // Execute users function
+            wait = true;              // Prevent future invocations
+            setTimeout(function () {  // After a period of time
+                wait = false;         // And allow future invocations
+            }, limit);
+        }
+    };
+}
